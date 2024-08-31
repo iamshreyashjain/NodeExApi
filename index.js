9dedfc0ec4eb2ef5571d36b408649283e8803e6e
Shreyash Jain <66334547+iamshreyashjain@users.noreply.github.com>
2024-08-31 22:54:00 +0000

    LinkPage

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require('express');
+const htmlPage = require('./')
 var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 var setupSwagger = require('./swagger');
-setupSwagger(app);  // Move this after the middleware
+setupSwagger(app);  // Ensure Swagger setup is after middleware
 
 const userRouter = require('./routes/usersRoutes');
 const productsRouter = require('./routes/productsRoutes');
 
-
 app.use('/users', userRouter);
 app.use('/products', productsRouter);
 
+app.get('/', (req, res) => {
+    res.send("<h1 style='color: blue; text-align: center;'>Hello, Swagger UI Page Link</h1> <a style='color: black; display:block ; text-align: center; text-decoration: none'; href = 'https://special-doodle-6wr49jv6w6vc56q9-3003.app.github.dev/shreyashJain/'>https://special-doodle-6wr49jv6w6vc56q9-3003.app.github.dev/shreyashJain/</a> ");
+});
 
 app.listen(3003, () => {
     console.log('Server started on http://localhost:3003');
-  });
-  
-  
+});
