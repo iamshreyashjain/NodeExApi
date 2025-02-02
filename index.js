9275bd5f7a859c019efc0cee13ef0c1ca877d34c
Shreyash Jain <66334547+iamshreyashjain@users.noreply.github.com>
2025-02-02 04:51:14 +0000

    corsError+

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
 const express = require('express');
 const htmlPage = require('./')
-
+var setupSwagger = require('./swagger');
 var app = express();
+const userRouter = require('./routes/usersRoutes');
+const productsRouter = require('./routes/productsRoutes');
+const cors = require('cors');
+app.use(cors());    
+
+
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
+const PORT = process.env.PORT || 3001;
 
-var setupSwagger = require('./swagger');
 
 setupSwagger(app);  // Ensure Swagger setup is after middleware
 
-const userRouter = require('./routes/usersRoutes');
 app.use('/users', userRouter);
 
-const productsRouter = require('./routes/productsRoutes');
 app.use('/products', productsRouter);
+
+app.use(cors({
+    origin: ['https://yourfrontenddomain.com', 'https://nodeexapi.onrender.com'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.get('/', (req, res) => {
     res.send(`
         <h1 style="color: black; text-align: center;">Hello, Swagger UI Page Link</h1>
         <a style="color: blue; display: block; text-align: center;" href="https://nodeexapi.onrender.com/swagger">Swagger Link</a>
     `);
-    
-
 });
 
-app.listen(3001, () => {
-    console.log('Server started on http://localhost:3001/swagger');
+app.use(cors({
+    origin: 'https://yourfrontenddomain.com'
+  }));
+  
+
+app.get('/', (req, res) => {
+    res.send(`
+        <h1 style="color: black; text-align: center;">Hello, Swagger UI Page Link</h1>
+        <a style="color: blue; display: block; text-align: center;" href="https://nodeexapi.onrender.com/swagger">Swagger Link</a>
+    `);
+});
+
+app.use(cors());
+
+
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}/swagger`);
 });
