eb4f24c8a5555c99b79f5473173a84004e5cd79a
iamshreyashjain <shreyashjain41@gmail.com>
2025-06-25 01:25:43 +0530

    changes+

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express');
+
 const htmlPage = require('./')
+
 var setupSwagger = require('./swagger');
+
 var app = express();
+
 const userRouter = require('./routes/usersRoutes');
+
 const productsRouter = require('./routes/productsRoutes');
+
 const cors = require('cors');
+
 app.use(cors());    
-
-
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
-const PORT = process.env.PORT || 3001;
 
+const PORT = process.env.PORT || 3001;
 
 setupSwagger(app);  // Ensure Swagger setup is after middleware
 
@@ -22,7 +27,7 @@ app.use('/users', userRouter);
 app.use('/products', productsRouter);
 
 app.use(cors({
-    origin: ['https://yourfrontenddomain.com', 'https://nodeexapi.onrender.com'],
+    origin: ['https://nodeexapi.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
@@ -34,17 +39,14 @@ app.get('/', (req, res) => {
     `);
 });
 
-app.use(cors({
-    origin: 'https://yourfrontenddomain.com'
-  }));
-  
 
-app.get('/', (req, res) => {
-    res.send(`
-        <h1 style="color: black; text-align: center;">Hello, Swagger UI Page Link</h1>
-        <a style="color: blue; display: block; text-align: center;" href="https://nodeexapi.onrender.com/swagger">Swagger Link</a>
-    `);
-});
+
+// app.get('/', (req, res) => {
+//     res.send(`
+//         <h1 style="color: black; text-align: center;">Hello, Swagger UI Page Link</h1>
+//         <a style="color: blue; display: block; text-align: center;" href="https://nodeexapi.onrender.com/swagger">Swagger Link</a>
+//     `);
+// });
 
 app.use(cors());
 
